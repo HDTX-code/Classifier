e172a7f5e84ef3994018484975fa6cb938c52590
HDTX-code <1252900424@qq.com>
2022-05-27 15:38:47 +0800

    修正

--- nets/metrics.py
+++ nets/metrics.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 def get_score(inputs, target):
     input = torch.argmax(inputs, dim=-1)
-    score = torch.sum(input == target)
+    score = torch.sum(input == target).type(torch.FloatTensor)
     return score.mean()
 
 
