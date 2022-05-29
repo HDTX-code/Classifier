01680dca4c9c6955d98fce34795fff3b3bbba39f
HDTX-code <1252900424@qq.com>
2022-05-29 19:16:33 +0800

    添加预测部分

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -81,25 +81,25 @@ def fit_one_epoch(model, optimizer, epoch_now, epoch_Freeze, num_classes,
                                             's': val_score / (iteration + 1),
                                             'r': get_lr(optimizer)})
                     pbar_val.update(1)
-        # 保存模型
-        with torch.no_grad():
-            print('Finish Validation')
-            loss_history.append_loss(epoch_now + 1, total_loss / len(gen), val_loss / len(gen_val))
-            print('Epoch:' + str(epoch_now + 1) + '/' + str(epoch_all))
-            print('Total Loss: %.6f || Val Loss: %.6f ' % (total_loss / len(gen), val_loss / len(gen_val)))
-            print('Total score: %.6f || Val score: %.6f ' % (
-                total_score / len(gen), val_score / len(gen_val)))
-            if ((epoch_now + 1) % interval == 0 or epoch_now + 1 == epoch_all) and epoch_now >= epoch_Freeze:
-                torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-score%.3f-val_score%.3f.pth' % (
-                    (epoch_now + 1), total_score / len(gen), val_score / len(gen_val))))
-    else:
-        with torch.no_grad():
-            print('Finish Validation')
-            loss_history.append_loss(epoch_now + 1, total_loss / len(gen), 0)
-            print('Epoch:' + str(epoch_now + 1) + '/' + str(epoch_all))
-            print('Total Loss: %.6f' % (total_loss / len(gen)))
-            print('Total score: %.6f ' % (total_score / len(gen)))
-            if ((epoch_now + 1) % interval == 0 or epoch_now + 1 == epoch_all) and epoch_now >= epoch_Freeze:
-                torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-score%.3f.pth' % (
-                    (epoch_now + 1), total_score / len(gen))))
+    #     # 保存模型
+    #     with torch.no_grad():
+    #         print('Finish Validation')
+    #         loss_history.append_loss(epoch_now + 1, total_loss / len(gen), val_loss / len(gen_val))
+    #         print('Epoch:' + str(epoch_now + 1) + '/' + str(epoch_all))
+    #         print('Total Loss: %.6f || Val Loss: %.6f ' % (total_loss / len(gen), val_loss / len(gen_val)))
+    #         print('Total score: %.6f || Val score: %.6f ' % (
+    #             total_score / len(gen), val_score / len(gen_val)))
+    #         if ((epoch_now + 1) % interval == 0 or epoch_now + 1 == epoch_all) and epoch_now >= epoch_Freeze:
+    #             torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-score%.3f-val_score%.3f.pth' % (
+    #                 (epoch_now + 1), total_score / len(gen), val_score / len(gen_val))))
+    # else:
+    #     with torch.no_grad():
+    #         print('Finish Validation')
+    #         loss_history.append_loss(epoch_now + 1, total_loss / len(gen), 0)
+    #         print('Epoch:' + str(epoch_now + 1) + '/' + str(epoch_all))
+    #         print('Total Loss: %.6f' % (total_loss / len(gen)))
+    #         print('Total score: %.6f ' % (total_score / len(gen)))
+    #         if ((epoch_now + 1) % interval == 0 or epoch_now + 1 == epoch_all) and epoch_now >= epoch_Freeze:
+    #             torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-score%.3f.pth' % (
+    #                 (epoch_now + 1), total_score / len(gen))))
     return model
